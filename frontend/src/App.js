7505fd2c7a48e84932e297077645f300cccf82fd
AJeraldJeyaSeelan <240251343@aston.ac.uk>
2025-07-07 02:19:43 +0100

    Added FE Directory and dependencies

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/registerPage.js';
+import Login from './pages/loginPage.js';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
